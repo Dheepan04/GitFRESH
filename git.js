258ffe56b84a1a0cd62270d8ea71356ce3d1789b
Dheepan04 <dheepangrasp@gmail.com>
2023-09-10 20:10:05 +0530

    I again made some changes on git.js

--- git.js
+++ git.js
@@ -1,6 +1,7 @@
 //Summa oru git//
 function hellp() {
     let x = 2; y = 3;
-    console.log(x + y);
-    return x + y;
+    console.log(x + x);
+    return x + x;
 }
+//I worked furiously and tremendously for committing changes pls merge it
